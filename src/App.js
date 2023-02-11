a30102e3a8e02d81b6ec781d767b89f7ce12018b
Jaypatel123 <jaypatel97043@gmail.com>
2023-02-11 12:47:10 -0500

    btns added

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import Die from "./Components/Die"
 
 import "./App.css";
 function App() {
-  const [diceNumber, isDiceNumber] = useState(1);
+  const [diceNumber, isDiceNumber] = useState(allNewDice());
+  function allNewDice(){
+      let newDice = []
+      for(let i=0; i<10; i++){
+        newDice.push(Math.ceil(Math.random() * 6))
+      }
+      return newDice
+  };
+
+  function diceRolled(){
+    
+  }
+
+  // const allNewDice = () => {
+  //   console.log(Math.floor(Math.random()* 6) + 1)
+  // }
+
+  const diceElements = diceNumber.map(die=><Die value={die}/>)
   return (
     <div className="App">
       <main>
-        <div>
-          <Die value='1'/>  
-          <Die value='1'/>        
-          <Die value='1'/>        
-          <Die value='1'/>        
-          <Die value='1'/>        
-          <Die value='1'/>        
-          <Die value='1'/>        
-          <Die value='1'/>        
-          <Die value='1'/>        
-          <Die value='1'/>        
+        <div className="dice-container">
+          {diceElements} 
         </div>
+        <button className="dice-rolled" onClick={diceRolled}> Roll</button>
       </main>
     </div>
   );
