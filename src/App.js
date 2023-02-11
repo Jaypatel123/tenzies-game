c1296902f733a78066965637e2841276aedcd951
Jaypatel123 <jaypatel97043@gmail.com>
2023-02-10 20:15:55 -0500

    adding value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { useState } from "react";
+import Die from "./Components/Die"
 
-import './App.css';
+import "./App.css";
 function App() {
+  const [diceNumber, isDiceNumber] = useState(1);
   return (
     <div className="App">
-      
+      <main>
+        <div>
+          <Die value='1'/>  
+          <Die value='1'/>        
+          <Die value='1'/>        
+          <Die value='1'/>        
+          <Die value='1'/>        
+          <Die value='1'/>        
+          <Die value='1'/>        
+          <Die value='1'/>        
+          <Die value='1'/>        
+          <Die value='1'/>        
+        </div>
+      </main>
     </div>
   );
 }
