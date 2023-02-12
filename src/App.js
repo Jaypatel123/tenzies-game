1a90a1af52b130a2e2f0517eb53780e170659237
Jaypatel123 <jaypatel97043@gmail.com>
2023-02-12 01:11:27 -0500

    adding holddice functionality

--- src/App.js
+++ src/App.js
@@ -5,23 +5,53 @@ import { nanoid } from "nanoid";
 import "./App.css";
 function App() {
   const [diceNumber, isDiceNumber] = useState(allNewDice());
+
+  function generateNewDice(){
+    return {
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false
+    }
+  }
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        id: nanoid(),
-        value: Math.ceil(Math.random() * 6),
-        isHeld: true
-      });
+      newDice.push(generateNewDice());
     }
     return newDice;
   }
 
   function diceRolled() {
-    isDiceNumber(allNewDice);
+
+    isDiceNumber(preNum => preNum.map(die=> die.isHeld ? die : generateNewDice()));
   }
+
+  function holdDice(id) {
+    isDiceNumber(preNum => {
+      return preNum.map(die => { 
+        return die.id === id ? 
+        {...die, isHeld: !die.isHeld} : 
+        die})
+    })
+
+    // diceNumber.map((die) =>
+    //   die.id === id
+    //     ? isDiceNumber((preVal) => [...preVal, die.isHeld = true])
+    //     : isDiceNumber((preVal) => preVal)
+    // );
+
+    console.log(id);
+  }
+
   const diceElements = diceNumber.map((die) => (
-    <Die key={die.id} value={die.value} isHeld = {die.isHeld}/>
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      id={die.id}
+      handleSubmit={() => holdDice(die.id)}
+    />
   ));
   return (
     <div className="App">
--- src/Components/Die.js
+++ src/Components/Die.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-
-export default function Die(props){
-    const styles = {
-        backgroundColor: props.isHeld ? "#59e351" : "white"
-      }
-    return(
-        <div style={styles} className="dice-face">
-            <h2 onClick={props.handleSubmit}>{props.value}</h2>
-        </div>
-    )
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59e351" : "white",
+  };
+  return (
+    <div style={styles} className="dice-face" onClick={props.handleSubmit}>
+      <h2>{props.value}</h2>
+    </div>
+  );
 }
